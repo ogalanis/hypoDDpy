907a17ab3714df01d3ab1b2ca55b735d1caa66b1
Tobias Megies <megies@users.noreply.github.com>
2016-12-06 14:05:39 +0100

    update obspy dependency in setup.py

--- setup.py
+++ setup.py
@@ -37,7 +37,10 @@ AUTHOR_EMAIL = 'krischer@geophysik.uni-muenchen.de'
 URL = 'None so far...'
 LICENSE = 'GNU General Public License, version 3 (GPLv3)'
 KEYWORDS = ['seismology', 'earthquakes', 'relocation']
-INSTALL_REQUIRES = ['obspy', 'progressbar']
+INSTALL_REQUIRES = [
+    'obspy>=1.1.0',
+    'progressbar',
+    ]
 ENTRY_POINTS = {}
 
 
